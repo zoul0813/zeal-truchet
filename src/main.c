1ae341c9aaca87266efa0799178faba61250efa5
David Higgins <zoul0813@me.com>
2025-07-10 09:03:11 -0400

    added simple patterns

--- src/main.c
+++ src/main.c
@@ -12,18 +12,18 @@
 
 const uint8_t CHARS[4][12] = {
     {
-        0b11111111,
-        0b01111111,
-        0b01111111,
-        0b00111111,
-        0b00011111,
-        0b00011111,
-        0b00001111,
-        0b00001111,
-        0b00000111,
-        0b00000011,
-        0b00000011,
         0b00000001,
+        0b00000011,
+        0b00000011,
+        0b00000111,
+        0b00001111,
+        0b00001111,
+        0b00011111,
+        0b00011111,
+        0b00111111,
+        0b01111111,
+        0b01111111,
+        0b11111111,
     },
     {
         0b10000000,
@@ -39,6 +39,21 @@ const uint8_t CHARS[4][12] = {
         0b11111110,
         0b11111111,
     },
+
+    {
+        0b11111111,
+        0b11111110,
+        0b11111110,
+        0b11111100,
+        0b11111000,
+        0b11111000,
+        0b11110000,
+        0b11110000,
+        0b11100000,
+        0b11000000,
+        0b11000000,
+        0b10000000,
+    },
     {
         0b11111111,
         0b01111111,
@@ -52,22 +67,17 @@ const uint8_t CHARS[4][12] = {
         0b00000011,
         0b00000011,
         0b00000001,
-    },
-    {
-        0b10000000,
-        0b11000000,
-        0b11000000,
-        0b11100000,
-        0b11110000,
-        0b11110000,
-        0b11111000,
-        0b11111000,
-        0b11111100,
-        0b11111110,
-        0b11111110,
-        0b11111111,
     },
 };
+
+#define PATTERN_LENGTH 4
+const uint8_t PATTERNS[][PATTERN_LENGTH] = {
+    { 2, 2, 2, 2 },
+    { 3, 2, 3, 2 },
+    { 3, 2, 1, 4 },
+};
+
+#define PATTERNS_LENGTH (sizeof(PATTERNS) / sizeof(PATTERNS[0]))
 
 const __sfr __banked __at(0xF2) mmu_page2_ro;
 __sfr __at(0xF2) mmu_page2;
@@ -93,17 +103,29 @@ inline void text_demap_vram(void)
 int main(void)
 {
     text_map_vram();
+    // backup existing font
     memcpy(font_backup, &FONT[12], 4 * 12);
+    // set new font chars
     memcpy(&FONT[12], CHARS, sizeof(CHARS));
+    text_demap_vram();
 
-    const char test[] = { 0x1 };
-    for(uint8_t y = 0; y < SCREEN_COL80_HEIGHT; y++) {
-        for(uint8_t x = 0; x < SCREEN_COL80_WIDTH; x++) {
-            printf("%s", test);
+    for(uint8_t p = 0; p < PATTERNS_LENGTH; p++) {
+        const uint8_t *test = PATTERNS[p];
+        for(uint8_t y = 0; y < 8; y++) {
+            for(uint8_t x = 0; x < SCREEN_COL80_WIDTH / PATTERN_LENGTH; x++) {
+                for(uint8_t c = 0; c < PATTERN_LENGTH; c++) {
+                    putchar(test[c]);
+                }
+            }
         }
+        printf("\n\n");
     }
 
+    // restore font backup
+    // text_map_vram();
     // memcpy(FONT, font_backup, 4 * 12);
-    text_demap_vram();
+    // text_demap_vram();
+
+    printf("patterns: %d\n", PATTERNS_LENGTH);
     return 0;
 }
